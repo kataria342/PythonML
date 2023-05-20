fc186cea48f18aef68bbb9ced8db38b057cd5c25
Prabhjeet Singh <kataria342@gmail.com>
2023-05-19 20:26:09 -0400

    NTLK Library

---
+++ AML2203/Lecture2/charsequence.py
@@ -0,0 +1,11 @@
+# strText = "abfdbwhwjnjdkrdsdfsf"
+
+# unique=[]
+# for i in strText:
+#     if i in unique:
+#         continue
+#     else:
+#         if int(i) < int(i+1):
+#             print(i)
+textStr = "a"
+print(int(textStr[0]))
